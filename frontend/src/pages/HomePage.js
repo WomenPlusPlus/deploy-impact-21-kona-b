930572febbf88ef6a84bcfc95c8d0cfe29a062a4
emilie <emi.m.humbert@googlemail.com>
2021-10-25 15:50:42 +0200

    Add react-router-dom and create HomePage

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function HomePage() {
+  return (
+    <div className="container">
+      <h1>Home Page</h1>
+    </div>
+  );
+}
