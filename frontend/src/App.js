5a09d826035cfd0ba31351786a2fa7704a1bc7bf
emilie <emi.m.humbert@googlemail.com>
2021-10-27 17:24:35 +0200

    Add navigation and button components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import Navigation from "./components/navigation";
 import HomePage from "./pages/HomePage";
 export default function App() {
   return (
+    <div className='m-5'>
+      <Navigation />
     <Switch>
       <Route exact path="/" component={HomePage} />
     </Switch>
+    </div>
   );
 }
 
