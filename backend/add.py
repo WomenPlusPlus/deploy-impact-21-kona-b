4b927bfa589267cc7bf1be52af6168a18a3fbff1
heba0 <hebamwagih@gmail.com>
2021-10-30 07:46:45 +0200

    add 'query' and 'add' APIs

---
+++ backend/add.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def add(data):
+    filename = 'organisations dataset.csv'
+    df = pd.read_csv(filename)
+    df = df.astype(str)
+    df2 = pd.DataFrame([data],
+                       columns=['Name of Organisation', 'Region', 'Can individuals contact them?'])
+    df = pd.concat([df2, df]).reset_index()
+    df.to_csv(filename, sep=',')
