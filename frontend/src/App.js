0e48077027643c23de744e334c6df5f7a49ac3d7
emilie <emi.m.humbert@googlemail.com>
2021-11-03 14:32:41 +0100

    Add organisations list page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,17 @@ import Navigation from "./components/navigation";
 import HomePage from "./pages/HomePage";
 import OrganisationsPage from "./pages/OrganisationsPage";
 import QuizPage from "./pages/QuizPage";
+import OrganisationsList from "./pages/OrganisationsListPage";
 
 export default function App() {
   return (
-    <div className='m-5'>
+    <div className="m-5">
       <Navigation />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/organisations" component={OrganisationsPage} />
         <Route path="/quiz" component={QuizPage} />
+        <Route path="/organisationslist" component={OrganisationsList} />
       </Switch>
     </div>
   );
