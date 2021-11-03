97f0efbb4dc064f26462aa242149a556bc69131a
emilie <emi.m.humbert@googlemail.com>
2021-11-03 14:52:13 +0100

    Rename organisations page

--- frontend/src/pages/OrganisationsPage.js
+++ frontend/src/pages/OrganisationsPage.js
@@ -3,14 +3,14 @@ import DocumentTitle from "react-document-title";
 
 import OrganisationsTable from "../components/organisationsTable";
 
-export default function OrganisationsList() {
+export default function OrganisationsPage() {
   return (
-    <DocumentTitle title="List of organisations">
+    <DocumentTitle title="Organisations">
       <main className="mx-7px">
         <div className="grid grid-cols-7 sm:grid-cols-7 max-w-xl lg:max-w-4xl mx-auto w-mobile mb-8 sm:mb-20">
           <div className="col-start-1 sm:col-start-2 sm:col-span-5 col-span-7">
             <h1 className="border-b border-kona col-span-3 my-8 text-2xl">
-              List of organisations
+              Organisations
             </h1>
             <p className="mb-6">
               Please find below the list of organisations. You can use our
