eec59c33dc758b5b05ac67c69353b8123d43da9d
heba0 <hebamwagih@gmail.com>
2021-11-05 16:52:38 +0200

    add search and add APIs

--- backend/add.py
+++ backend/add.py
@@ -1,10 +1,17 @@
 import pandas as pd
 
+
 def add(data):
     filename = 'organisations dataset.csv'
-    df = pd.read_csv(filename,index_col = False)
+    keys = []
+    values = []
+    for key, value in data.items():
+        keys.append(key)
+        values.append(value)
+
+    df = pd.read_csv(filename, index_col=False)
     df = df.astype(str)
-    df2 = pd.DataFrame([data],
-                       columns=['Name of Organisation', 'Region', 'Can individuals contact them?'])
+    df2 = pd.DataFrame([values],
+                       columns=keys)
     df = pd.concat([df2, df]).reset_index(drop=True)
     df.to_csv(filename, sep=',', index=False)
--- backend/api.py
+++ backend/api.py
@@ -1,11 +1,12 @@
 import flask
 from flask import request, jsonify
-from query import query
 from add import add
+from search import search
 import ast
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+
 
 @app.route('/', methods=['GET'])
 def home():
@@ -18,21 +19,13 @@ def page_not_found(e):
     return "<h1>404</h1><p> 404 :/ </p>", 404
 
 
-
-@app.route('/dots/organisations/query', methods=['POST'])
-def query_organisations():
-    body = ast.literal_eval(request.data.decode('utf-8'))
-    res = query(body['filters'])
+@app.route('/api/v0/organisations/search', methods=['GET'])
+def search_organisations():
+    filter = {}
+    filter['region'] = request.args['region']
+    filter['contactable'] = request.args['contactable']
+    res = search(filter)
     return jsonify(res), 201
-
-# @app.route('/api/v0/organisations/search', methods=['GET'])
-# def search_organisations():
-#     args = request.args
-#     print(args)
-#     body = ast.literal_eval(request.data.decode('utf-8'))
-#     res = query(body['filters'])
-#     return jsonify(res), 201
-
 
 
 @app.route('/dots/organisations/add', methods=['POST'])
