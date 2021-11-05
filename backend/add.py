b66efcbcede7ee7390aac0b25528c3e969480059
heba0 <hebamwagih@gmail.com>
2021-11-05 11:36:35 +0200

    bug fix

--- backend/add.py
+++ backend/add.py
@@ -2,9 +2,9 @@ import pandas as pd
 
 def add(data):
     filename = 'organisations dataset.csv'
-    df = pd.read_csv(filename)
+    df = pd.read_csv(filename,index_col = False)
     df = df.astype(str)
     df2 = pd.DataFrame([data],
                        columns=['Name of Organisation', 'Region', 'Can individuals contact them?'])
-    df = pd.concat([df2, df]).reset_index()
+    df = pd.concat([df2, df]).reset_index(drop=True)
     df.to_csv(filename, sep=',', index=False)
