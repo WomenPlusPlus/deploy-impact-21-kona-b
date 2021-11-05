a2e9f709629fcd9f7177dd5658c828e339a9302d
emilie <emi.m.humbert@googlemail.com>
2021-11-05 09:54:07 +0100

    Add language buttons in navigation

--- frontend/src/components/navigation.js
+++ frontend/src/components/navigation.js
@@ -1,15 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
 import Button from "../components/button";
+import LanguageButtons from "./languageButtons";
 
 export default function Navigation() {
   return (
-    <nav className="border-b-2 border-kona">
-      <div className="relative flex items-end justify-between mt-8 mb-8">
+    <nav className="border-b-2 border-kona mx-2 lg:mx-6 p-6">
+      <div className="relative flex items-end justify-between mx-auto max-w-xl lg:max-w-4xl mt-8 space-x-2 sm:space-x-18 lg:space-x-30">
         <Link to="/" className="text-md sm:text-3xl text-kona">
           Dots. logo
         </Link>
-        <Button description={"menu"} url={''}/>
+        <div className="flex flex-col justify-between">
+          <div className="absolute bottom-10 right-0">
+            <LanguageButtons />
+          </div>
+          <div>
+            <Button
+              description={"menu"}
+              url={""}
+            />
+          </div>
+        </div>
       </div>
     </nav>
   );
