aa44f76686f8e6532fdb8b4d2c36a1ea4fb1c99a
emilie <emi.m.humbert@googlemail.com>
2021-11-11 16:47:47 +0100

    Hide empty filters

--- frontend/src/components/checkboxFilter.js
+++ frontend/src/components/checkboxFilter.js
@@ -34,7 +34,6 @@ export default function OrganisationFilters({ title, allValues, setSelected }) {
           />
           <label
             className="cursor-pointer select-none"
-            key={value}
             htmlFor={`filter-${title}-${value}`}
           >
             {value}
