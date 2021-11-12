33c05512291acdfb0ef4d6a519f79df144da0a7b
emilie <emi.m.humbert@googlemail.com>
2021-11-12 11:34:23 +0100

    Add contact details to the OrganisationPage

---
+++ frontend/src/components/map.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Map() {
+  return (
+    <div className="bg-gray-200 flex flex-wrap h-44 my-2 place-content-center">
+      MAP
+    </div>
+  );
+}
