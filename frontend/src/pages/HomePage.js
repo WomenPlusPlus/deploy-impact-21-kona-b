606f413aaff8214ff01c3919df5c7aeee72807e9
emilie <emi.m.humbert@googlemail.com>
2021-11-15 16:46:14 +0100

    Update Homepage layout

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -5,29 +5,35 @@ import { useTranslation } from "react-i18next";
 import Button from "../components/button";
 
 export default function HomePage() {
-  const { t, i18n } = useTranslation("home");
+  const { t } = useTranslation("home");
 
   return (
     <DocumentTitle title="homepage">
       <div>
-        <div className="text-center my-12">
-          <h2 className="font-medium justify-center leading-loose mb-20 sm:mb-24 lg:mb-32 mt-14 sm:mt-20 lg:mt-24 text-3xl sm:text-6xl">
-            {t("title")}
+        <div className="text-center">
+          <p className="italic leading-relaxed text-xs">{t("presentation")}</p>
+          <h2 className="font-medium justify-center leading-loose my-12 sm:my-24 lg:my-28 text-xl sm:text-3xl">
+            {t("title")} <br /> Dots.
           </h2>
-          <p className="leading-relaxed my-20 sm:my-20 lg:my-24 text-xl sm:text-3xl">
-            {t("presentation")}
-          </p>
-          <div className="gap-16 grid sm:grid-cols-2 m-8">
+          <p className="mb-12 sm:mb-20 text-lg">{t("subTitle")}</p>
+          <div className="gap-8 sm:gap-16 grid sm:grid-cols-2 m-8">
             <div>
-              <p className="mb-2">{t("textListOrganisations")}</p>
               <Button
                 description={t("listOrganisationButton")}
                 url={"organisations"}
+                height={"h-28"}
+                width={"w-52"}
               />
+              <p className="my-2 text-xs">{t("textListOrganisations")}</p>
             </div>
             <div>
-              <p className="mb-2">{t("textQuiz")}</p>
-              <Button description={t("quizButton")} url={"quiz"} />
+              <Button
+                description={t("quizButton")}
+                url={"quiz"}
+                height={"h-28"}
+                width={"w-52"}
+              />
+              <p className="my-2 text-xs">{t("textQuiz")}</p>
             </div>
           </div>
         </div>
