d44fb02969608a05b9b95d88076569f0d72ba28b
emilie <emi.m.humbert@googlemail.com>
2021-11-15 16:46:14 +0100

    Update Homepage layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { Suspense } from "react";
 import { Route, Switch } from "react-router-dom";
+
 import Navigation from "./components/navigation";
 import HomePage from "./pages/HomePage";
 import OrganisationPage from "./pages/OrganisationPage";
@@ -11,13 +12,17 @@ export default function App() {
   return (
     <Suspense fallback="loading">
       <Navigation />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/organisations/:id" component={OrganisationPage} />
-        <Route path="/quiz" component={QuizPage} />
-        <Route path="/organisations" component={OrganisationsPage} />
-        <Route path="/organisationForm" component={OrganisationFormPage} />
-      </Switch>
+      <div className="grid grid-cols-9 sm:grid-cols-7 max-w-xl lg:max-w-4xl mx-auto w-mobile mb-8 sm:mb-20">
+        <div className="col-start-1 sm:col-start-1 col-span-9 sm:col-span-7 mx-2 sm:mx-0">
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/organisations/:id" component={OrganisationPage} />
+            <Route path="/quiz" component={QuizPage} />
+            <Route path="/organisations" component={OrganisationsPage} />
+            <Route path="/organisationForm" component={OrganisationFormPage} />
+          </Switch>
+        </div>
+      </div>
     </Suspense>
   );
 }
--- frontend/src/components/button.js
+++ frontend/src/components/button.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import classNames from "classnames";
 
-export default function Button({ description, url }) {
+export default function Button({ description, url, height, width }) {
   return (
     <button
-      className="border border-kona px-4 py-2 rounded uppercase"
+      className={classNames(
+        `border border-kona px-4 py-2 rounded uppercase ${height} ${width}`
+      )}
       type="button"
     >
       <Link to={`/${url}`}>{description}</Link>
