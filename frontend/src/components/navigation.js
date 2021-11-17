d31baff519ba4a3bb0e428d2dc84d6d75a93951e
emilie <emi.m.humbert@googlemail.com>
2021-11-17 23:00:36 +0100

    Update wording and layout of Homepage

--- frontend/src/components/navigation.js
+++ frontend/src/components/navigation.js
@@ -1,24 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import Button from "../components/button";
+import LinkButton from "./LinkButton";
 import LanguageButtons from "./languageButtons";
 
 export default function Navigation() {
   return (
-    <nav className="border-b-2 border-kona mx-2 lg:mx-6 p-6">
-      <div className="relative flex items-end justify-between mx-auto max-w-xl lg:max-w-4xl mt-8 space-x-2 sm:space-x-18 lg:space-x-30">
+    <nav className="border-b-2 border-kona mx-2 lg:mx-6">
+      <div className="relative flex items-end justify-between mx-auto max-w-xl lg:max-w-4xl mt-6 space-x-2 sm:space-x-18 lg:space-x-30">
         <Link to="/" className="text-md sm:text-3xl text-kona">
           Dots. logo
         </Link>
         <div className="flex flex-col justify-between">
-          <div className="absolute bottom-10 right-0">
+          <div className="absolute bottom-5 right-0">
             <LanguageButtons />
           </div>
           <div>
-            <Button
+            <LinkButton
               description={"menu"}
               url={""}
+              fontSize={""}
+              padding={""}
+              margin={""}
             />
           </div>
         </div>
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -2,41 +2,54 @@ import React from "react";
 import DocumentTitle from "react-document-title";
 import { useTranslation } from "react-i18next";
 
-import Button from "../components/button";
+import LinkButton from "../components/LinkButton";
 
 export default function HomePage() {
   const { t } = useTranslation("home");
 
   return (
     <DocumentTitle title="homepage">
-      <div>
+      <div className=" mx-6">
         <div className="text-center">
-          <p className="italic leading-relaxed text-xs">{t("presentation")}</p>
-          <h2 className="font-medium justify-center leading-loose my-12 sm:my-24 lg:my-28 text-xl sm:text-3xl">
-            {t("title")} <br /> Dots.
+          <h2 className="font-medium justify-center leading-loose my-2 sm:my-24 md:my-12 text-2xl md:text-4xl">
+            {t("welcome")}
           </h2>
-          <p className="mb-12 sm:mb-20 text-lg">{t("subTitle")}</p>
-          <div className="gap-8 sm:gap-16 grid sm:grid-cols-2 m-8">
+          <p className="text-xs md:text-lg tracking-tight">
+            {t("presentation")}
+          </p>
+          <div className="bg-gray-200 flex flex-wrap h-28 my-2 place-content-center">
+            Image
+          </div>
+          <p className="m-6 lg:my-16 sm:mb-20 text-xl md:text-3xl">
+            {t("title")}
+          </p>
+          <p className="m-2 lg:mb-6 sm:mb-12 text-xs md:text-xl">
+            {t("subTitle")}
+          </p>
+          <div className="gap-4 sm:gap-16 grid sm:grid-cols-2">
             <div>
-              <Button
-                description={t("listOrganisationButton")}
-                url={"organisations"}
-                height={"h-28"}
-                width={"w-52"}
-              />
-              <p className="my-2 text-xs">{t("textListOrganisations")}</p>
-            </div>
-            <div>
-              <Button
+              <LinkButton
                 description={t("quizButton")}
                 url={"quiz"}
-                height={"h-28"}
-                width={"w-52"}
+                fontSize={"text-xs"}
+                padding={"px-4 py-4"}
+                margin={"mx-2"}
               />
-              <p className="my-2 text-xs">{t("textQuiz")}</p>
+            </div>
+            <div>
+              <LinkButton
+                description={t("listOrganisationButton")}
+                url={"organisations"}
+                fontSize={"text-xs"}
+                padding={"px-2 py-4"}
+                margin={"mx-2"}
+              />
             </div>
           </div>
         </div>
+        <p className="m-4 text-xs md:text-base text-start">
+          {t("privacyPolicy")}
+        </p>
       </div>
     </DocumentTitle>
   );
