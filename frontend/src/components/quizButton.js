738cd02b2bdc816f8244e93742268e48a765a761
emilie <emi.m.humbert@googlemail.com>
2021-11-19 14:16:01 +0100

    Add icons to answer buttons

--- frontend/src/components/quizButton.js
+++ frontend/src/components/quizButton.js
@@ -1,18 +1,25 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 
-export default function QuizButton({ translationKey, setValue, active }) {
+export default function QuizButton({ translationKey, setValue, active, Icon }) {
   const { t } = useTranslation("quiz");
 
   const classNames =
-    "border border-gray-200 hover:bg-gray-200 sm:mr-6 px-4 py-2 rounded text-sm uppercase";
+    "border border-gray-200 hover:bg-gray-200 sm:mr-6 px-4 py-2 rounded text-xs uppercase";
+
   return (
-    <button
-      className={active ? `${classNames} border-kona` : classNames}
-      type="button"
-      onClick={() => setValue()}
-    >
-      {t(translationKey)}
-    </button>
+    <>
+      <button
+        className={active ? `${classNames} border-kona` : classNames}
+        type="button"
+        onClick={() => setValue()}
+      >
+        <div className="text-xl flex justify-center mb-2">
+          {Icon && <Icon />}
+        </div>
+
+        {t(translationKey)}
+      </button>
+    </>
   );
 }
