6129c73e6b3cb7639c4b428cdab3d81c67963b5c
emilie <emi.m.humbert@googlemail.com>
2021-11-19 20:59:03 +0100

    Save answer on button click for single answer questions

--- frontend/src/components/submitButton.js
+++ frontend/src/components/submitButton.js
@@ -1,6 +1,12 @@
 import React from "react";
 
-export default function SubmitButton({ text, textLast, step, onClick }) {
+export default function SubmitButton({
+  text,
+  textLast,
+  step,
+  onClick,
+  numberQuestions,
+}) {
   return (
     <button
       className="border border-kona hover:bg-gray-200 my-6 px-4 py-2 rounded uppercase"
@@ -8,7 +14,7 @@ export default function SubmitButton({ text, textLast, step, onClick }) {
       onClick={() => onClick()}
     >
       {/* on the last question the button message will be 'next' otherwise it will be 'submit */}
-      {step >= 5 ? textLast : text}
+      {step >= numberQuestions - 1 ? textLast : text}
     </button>
   );
 }
--- frontend/src/pages/QuizPage.js
+++ frontend/src/pages/QuizPage.js
@@ -16,9 +16,11 @@ export default function QuizPage() {
   const [allAnswer, setAllAnswer] = useState([]);
 
   const [step, setStep] = useState(0);
+  const numberQuestions = 5;
 
   // when user can have more than one answer, makes an array with multiple answers
   // which will be deleted when they untick the answer
+  // when user can only have one answer, save the value and go to the next question
   const handleClickAnswer = (type, value) => {
     if (type === "checkbox") {
       if (Array.isArray(answer) && answer.includes(value)) {
@@ -30,14 +32,18 @@ export default function QuizPage() {
       }
     } else {
       setAnswer(value);
+      setAllAnswer([...allAnswer, [quiz[step].scope, answer]]);
+      if (step < numberQuestions) {
+        setStep(step + 1);
+      }
     }
   };
 
   // on click update the array of all answers (will be send to backend)
   // and increment step by 1 to get the next question
-  const handleClick = () => {
+  const handleClickNext = () => {
     setAllAnswer([...allAnswer, [quiz[step].scope, answer]]);
-    if (step <= 4) {
+    if (step < numberQuestions) {
       setStep(step + 1);
     }
   };
@@ -52,34 +58,69 @@ export default function QuizPage() {
     <DocumentTitle title="Quiz">
       <main className="mx-7px">
         <div className="grid grid-cols-7 sm:grid-cols-7 max-w-xl lg:max-w-4xl mx-auto w-mobile mb-8 sm:mb-20">
-          <div className="col-start-1 sm:col-start-2 sm:col-span-5 col-span-7 mx-4 sm:mx-0">
-            {step < 1 && <p className="my-6 text-sm">{t("presentation")}</p>}
-            <h2 className="my-6 text-lg font-semibold uppercase">
-              {t(`${quiz[step].scope.filter}.question`)}
-            </h2>
-            <div>
-              {/* when the type is checkbox, it display the Checkbox component */}
-              {quiz[step].type === "checkbox" ? (
-                <div>
-                  {quiz[step].answers.map((filterKey) => {
-                    return (
-                      <div key={filterKey.value}>
-                        <CheckboxButton
+          {step < numberQuestions ? (
+            <div className="col-start-1 sm:col-start-2 sm:col-span-5 col-span-7 mx-4 sm:mx-0">
+              {step < 1 && <p className="my-6 text-sm">{t("presentation")}</p>}
+              <h2 className="my-6 text-lg font-semibold uppercase">
+                {t(`${quiz[step].scope.filter}.question`)}
+              </h2>
+              <div>
+                {/* when the type is checkbox, it display the Checkbox component */}
+                {quiz[step].type === "checkbox" ? (
+                  <div>
+                    {quiz[step].answers.map((filterKey) => {
+                      return (
+                        <div key={filterKey.value}>
+                          <CheckboxButton
+                            translationKey={`${quiz[step].scope.filter}.filters.${filterKey.text}`}
+                            setValue={() =>
+                              handleClickAnswer(
+                                quiz[step].type,
+                                filterKey.value
+                              )
+                            }
+                            active={answer === filterKey.value}
+                            Icon={iconMap[filterKey.text]}
+                          />
+                        </div>
+                      );
+                    })}
+                  </div>
+                ) : (
+                  // else it display the Quizbutton component
+                  <div className="grid grid-cols-2 gap-4">
+                    {quiz[step].answers.map((filterKey) => {
+                      return (
+                        <QuizButton
+                          key={filterKey.value}
                           translationKey={`${quiz[step].scope.filter}.filters.${filterKey.text}`}
                           setValue={() =>
                             handleClickAnswer(quiz[step].type, filterKey.value)
                           }
+                          value={answer}
                           active={answer === filterKey.value}
-                            Icon={iconMap[filterKey.text]}
-            <div className="flex justify-end">
-              <SubmitButton
-                text={t("submitButton")}
-                textLast={t("submitButtonLast")}
-                step={step}
-                onClick={() => handleClick(answer)}
-              />
+                          Icon={iconMap[filterKey.text]}
+                        />
+                      );
+                    })}
+                  </div>
+                )}
+              </div>
+              {quiz[step].type === "checkbox" && (
+                <div className="flex justify-end">
+                  <SubmitButton
+                    text={t("submitButton")}
+                    textLast={t("submitButtonLast")}
+                    step={step}
+                    onClick={() => handleClickNext(answer)}
+                    numberQuestions={numberQuestions}
+                  />
+                </div>
+              )}
             </div>
-          </div>
+          ) : (
+            <div>Hello</div>
+          )}
         </div>
       </main>
     </DocumentTitle>
