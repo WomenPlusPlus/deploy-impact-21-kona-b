98e1e3369a8284ba1e3ea2d2d5ff2efc07c1d7ab
heba0 <hebamwagih@gmail.com>
2021-11-21 06:08:13 +0200

    add translation keys

--- backend/quiz.py
+++ backend/quiz.py
@@ -17,12 +17,12 @@ def get_questions():
     quiz = []
     for i in q_js:
         res = {}
-        res["scope"] = {"id": q_js[i]["q_id"], "text": q_js[i]["question"], "filter": q_js[i]["filter"]}
+        res["scope"] = {"id": q_js[i]["q_id"], "text": q_js[i]["question"], "filter": q_js[i]["filter"],"translation_key":q_js[i]["translation_key"]}
         res["type"] = q_js[i]["format"]
         ans = []
         for j in a_js:
             if (a_js[j]["q_id"] == q_js[i]["q_id"]):
-                ans.append({"id": a_js[j]["a_id"], "text": a_js[j]["answers"], "value": a_js[j]["filter_value"]})
+                ans.append({"id": a_js[j]["a_id"], "text": a_js[j]["answers"], "value": a_js[j]["filter_value"],"translation_key":a_js[j]["translation_key"]})
         res["answers"] = ans
         quiz.append(res)
     return quiz
