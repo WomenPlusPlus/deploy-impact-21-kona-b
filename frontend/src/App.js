d8279a8e6f7e4027b603cdc6b20b504b40d0d6f8
emilie <emi.m.humbert@googlemail.com>
2021-11-22 14:49:49 +0100

    Add drop down menu with links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,9 +13,9 @@ import AboutUsPage from "./pages/AboutUsPage";
 export default function App() {
   return (
     <Suspense fallback="loading">
-      <Navigation />
       <div className="grid grid-cols-9 sm:grid-cols-7 max-w-xl lg:max-w-4xl mx-auto w-mobile mb-8 sm:mb-20">
         <div className="col-start-1 sm:col-start-1 col-span-9 sm:col-span-7 mx-2 sm:mx-0">
+          <Navigation />
           <Switch>
             <Route exact path="/" component={HomePage} />
             <Route path="/organisations/:id" component={OrganisationPage} />
