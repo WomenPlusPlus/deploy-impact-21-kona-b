cc3572d77b96e0af73dc4ed29bdf733d17ee656f
emilie <emi.m.humbert@googlemail.com>
2021-11-24 18:14:20 +0100

    Fix drop down menu

--- frontend/src/components/navigation.js
+++ frontend/src/components/navigation.js
@@ -16,11 +16,11 @@ export default function Navigation() {
   ];
   const [dropDown, setDropDown] = useState(false);
 
+  // on click the dropdown menu opens or closes
   const handleClick = () => {
     setDropDown(!dropDown);
   };
 
-  console.log(dropDown);
   return (
     <nav className="border-b border-kona">
       <div className="flex h-20 items-end justify-between pb-2 relative">
@@ -40,7 +40,11 @@ export default function Navigation() {
               <div onClick={handleClick} className="fixed inset-0"></div>
               <div className="absolute bg-white flex-col border-b border-kona left-0 sm:left-auto m-px right-0 shadow-2xl top-20 z-50">
                 {pages.map(([page, url]) => (
-                  <Link to={`/${url}`} key={page}>
+                  <Link
+                    to={`/${url}`}
+                    key={page}
+                    onClick={() => setDropDown(!dropDown)}
+                  >
                     <div className="pb-2 pt-2 px-4 uppercase focus:outline-none hover:text-gray-500">
                       {t(`${page}`)}
                     </div>
