7b51b7c4c671d3f121ff6389bbf0ea09f94d0053
emilie <emi.m.humbert@googlemail.com>
2021-11-25 18:44:58 +0100

    Change class to className on progressBar component

--- frontend/src/components/progressBar.js
+++ frontend/src/components/progressBar.js
@@ -8,14 +8,14 @@ export default function ProgressBar({ step }) {
   const { t } = useTranslation("quiz");
   
   return (
-    <div class="container">
-      <ul class="progressbar">
-        <li class="active">{t("start")}</li>
-        <li class={classNames({ 'active': step+1 >= 2 })}></li>
-        <li class={classNames({ 'active': step+1 >= 3 })}></li>
-        <li class={classNames({ 'active': step+1 >= 4 })}></li>
-        <li class={classNames({ 'active': step+1 >= 5 })}></li>
-        <li class={classNames({ 'active': step+1 >= 6 })}></li>
+    <div className="container">
+      <ul className="progressbar">
+        <li className="active">{t("start")}</li>
+        <li className={classNames({ 'active': step+1 >= 2 })}></li>
+        <li className={classNames({ 'active': step+1 >= 3 })}></li>
+        <li className={classNames({ 'active': step+1 >= 4 })}></li>
+        <li className={classNames({ 'active': step+1 >= 5 })}></li>
+        <li className={classNames({ 'active': step+1 >= 6 })}></li>
         <li >{t("end")}</li>
       </ul>
     </div>
