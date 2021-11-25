d5b544ff641da08e4ca5b3755f6ca665d03ff6ad
emilie <emi.m.humbert@googlemail.com>
2021-11-25 23:55:36 +0100

    Modify design homepage

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import DocumentTitle from "react-document-title";
 import { useTranslation } from "react-i18next";
 
-import LinkButton from "../components/LinkButton";
+import LinkButton from "../components/linkButton";
 
 export default function HomePage() {
   const { t } = useTranslation("home");
@@ -12,18 +12,17 @@ export default function HomePage() {
     <DocumentTitle title="homepage">
       <>
         <div className="text-center">
-          <p className="my-2 text-xs md:text-lg tracking-tight">
-            {t("presentation")}
-          </p>
-          <div className="flex bg-tree-made-with-hands bg-cover h-28 lg:h-60 justify-end md:justify-center">
-            <h2 className="sm:my-6 md:my-12 mr-8 sm:ml-96 self-center text-2xl lg:text-5xl z-40">
+          <p className="my-2 text-xs tracking-tight">{t("presentation")}</p>
+          <div className="grid grid-cols-2 ">
+            <div className="bg-colourful-tree bg-contain bg-center bg-no-repeat h-32 md:h-72"></div>
+            <h2 className="self-center text-xl sm:text-4xl lg:text-6xl">
               {t("welcome.0")} <br />
               {t("welcome.1")}
               <br />
               {t("welcome.2")}
             </h2>
           </div>
-          <p className="my-6 lg:my-16 sm:mb-20 text-xl md:text-3xl">
+          <p className="my-6 lg:mt-16 lg:mb-8 sm:mb-8 text-xl md:text-3xl">
             {t("title")}
           </p>
           <div className="gap-4 sm:gap-16 grid sm:grid-cols-2">
@@ -31,28 +30,30 @@ export default function HomePage() {
               <LinkButton
                 description={t("quizButton")}
                 url={"quiz"}
-                fontSize={"text-xs"}
+                fontSize={""}
                 padding={"px-4 py-4"}
                 margin={"mx-0"}
-                uppercase={"uppercase"}
+                border={"border-2  border-konaInspiredDark"}
               />
             </div>
             <div>
               <LinkButton
                 description={t("listOrganisationButton")}
                 url={"organisations"}
-                fontSize={"text-xs"}
+                fontSize={""}
                 padding={"px-2 py-4"}
                 margin={"mx-0"}
-                uppercase={"uppercase"}
+                border={"border-2  border-konaInspiredDark"}
               />
             </div>
           </div>
         </div>
-        <div className="p-6 sm:pb-24 w-full text-xs text-center">
-              <div>{t("privacyPolicy.0")}</div>
-              <Link  className="underline" to="/privacy-policy">{t("privacyPolicy.1")}</Link>
-            </div>
+        <div className="p-6 sm:pb-24 md:mt-8 w-full text-xs text-center">
+          <div>{t("privacyPolicy.0")}</div>
+          <Link className="underline" to="/privacy-policy">
+            {t("privacyPolicy.1")}
+          </Link>
+        </div>
       </>
     </DocumentTitle>
   );
