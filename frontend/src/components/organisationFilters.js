89deb16460817fac188a7c58a4e9b4b929e6844c
emilie <emi.m.humbert@googlemail.com>
2021-11-26 12:16:37 +0100

    Add SDGs/Categories to filter and table

--- frontend/src/components/organisationFilters.js
+++ frontend/src/components/organisationFilters.js
@@ -2,30 +2,42 @@ import React, { useState, useEffect } from "react";
 
 import CheckboxFilter from "./checkboxFilter";
 
-// SDG will be replaced by categories
-
 export default function OrganisationFilters({
   organisations,
   setFilteredOrganisations,
 }) {
+  // get all regions from organisations
   const allRegions = [
     ...new Set(organisations.flatMap(({ region }) => region || [])),
   ];
+  // selected regions state
   const [selectedRegions, setSelectedRegions] = useState([]);
 
-  const allSdg = [...new Set(organisations.flatMap(({ sdg }) => sdg || []))];
+  // get all SDGs/categories from organisations
+  const allSdg = [
+    ...new Set(
+      organisations.flatMap(({ dots_categories }) => dots_categories || [])
+    ),
+  ];
+  // selected SDGs/categories state
   const [selectedSdgs, setSelectedSdgs] = useState([]);
 
   useEffect(() => {
-    const filteredOrganisations = organisations.filter(
-      ({ region, sdg }) =>
-        (selectedRegions.length === 0 ||
-          selectedRegions.some((selectedRegion) =>
-            region.includes(selectedRegion)
-          )) &&
-        (selectedSdgs.length === 0 ||
-          selectedSdgs.some((selectedSdg) => sdg.includes(selectedSdg)))
-    );
+    const filteredOrganisations = organisations.filter((organisation) => {
+      // filter the regions and make sure they don't appears twice
+      const matchesRegion =
+        selectedRegions.length === 0 ||
+        selectedRegions.some((selectedRegion) =>
+          organisation.region.includes(selectedRegion)
+        );
+      // filter the SDGs/categories and make sure they don't appears twice
+      const matchesSdg =
+        selectedSdgs.length === 0 ||
+        selectedSdgs.some((selectedSdg) =>
+          organisation.dots_categories.includes(selectedSdg)
+        );
+      return matchesRegion && matchesSdg;
+    });
     setFilteredOrganisations(filteredOrganisations);
   }, [organisations, setFilteredOrganisations, selectedRegions, selectedSdgs]);
 
@@ -33,14 +45,16 @@ export default function OrganisationFilters({
     <div>
       {allRegions.length > 0 && (
         <CheckboxFilter
-          title="Regions"
+          title="regions"
+          translationKeyPrefix="region.filters"
           allValues={allRegions}
           setSelected={setSelectedRegions}
         />
       )}
       {allSdg.length > 0 && (
         <CheckboxFilter
-          title="sdg"
+          title="support services"
+          translationKeyPrefix="dots_categories.filters"
           allValues={allSdg}
           setSelected={setSelectedSdgs}
         />
