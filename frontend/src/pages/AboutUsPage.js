2f14365b027c6f9006e81c39d4161ad0ec19ca31
emilie <emi.m.humbert@googlemail.com>
2021-11-26 15:46:22 +0100

    Update AboutUs page

--- frontend/src/pages/AboutUsPage.js
+++ frontend/src/pages/AboutUsPage.js
@@ -6,45 +6,42 @@ export default function AboutUsPage() {
   const { t } = useTranslation("aboutUs");
   const paragraphs = ["text.0", "text.1"];
   const links = [
-    "https://kona-connect.org/",
-    "https://kona-connect.org/dots-legal-aid-tech/",
+    "https://kona-connect.org/dots-legal-aid-tech",
     "https://www.womenplusplus.ch/deploy-impact",
   ];
 
   return (
     <DocumentTitle title="Privacy policy">
-      <main className="mx-7px">
-            <h1
-              className="mb-6 mt-12
+      <main className="mx-7px leading-relaxed">
+        <h1
+          className="mb-6 mt-12
              text-3xl uppercase"
-            >
-              {t("title")}
-            </h1>
-            {paragraphs.map((paragraph) => (
-              <p className="mb-6 leading-relaxed" key={paragraph}>
-                {t(paragraph)}
-              </p>
-            ))}
-            <p>
-              <Trans i18nKey="contact" t={t}>
-                If you want to contact Kona Connect, please go{" "}
-                <a
-                  href="https://kona-connect.org/contact/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  here
-                </a>
-                .
-              </Trans>
-            </p>
-            {links.map((link) => (
-              <p key={link} className="mt-2">
-                <a href={link} target="_blank" rel="noreferrer">
-                  {link}
-                </a>
-              </p>
-            ))}
+        >
+          {t("title")}
+        </h1>
+        {paragraphs.map((paragraph) => (
+          <p className="mb-6" key={paragraph}>
+            {t(paragraph)}
+          </p>
+        ))}
+        <p>{t("contact")}</p>
+        <p className="mb-6">
+          <a
+            href="https://kona-connect.org/contact"
+            target="_blank"
+            rel="noreferrer"
+          >
+            https://kona-connect.org/contact
+          </a>
+        </p>
+        <p>{t("moreInfo")}</p>
+        {links.map((link) => (
+          <p key={link}>
+            <a href={link} target="_blank" rel="noreferrer">
+              {link}
+            </a>
+          </p>
+        ))}
       </main>
     </DocumentTitle>
   );
