0db5f368607850ec35c969b1cf610e9affad1be7
emilie <emi.m.humbert@googlemail.com>
2021-11-26 16:40:35 +0100

    Add allowed origins to deploy script

--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -2,3 +2,8 @@
 
 # deploy code
 az webapp up
+if [ $? -ne 0 ]; then
+  exit $?
+fi
+# add allowed origins
+az webapp cors add --allowed-origins "https://dots.azureedge.net" "https://konaconnectdots.z1.web.core.windows.net"
